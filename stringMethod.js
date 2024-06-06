f8284f60046a342c1aafc28b29f59ea545c6747b
darpann10 <darpansapkota14@gmail.com>
2024-06-05 19:26:11 -0700

    Add files via upload

---
+++ stringMethod.js
@@ -0,0 +1,20 @@
+
+var text="This is BSC.CSIT fifth sem lab work";
+// AlertText
+alert(text);
+console.log(text);
+
+// Print the length of the string
+console.log("the length of the text is:",
+text.length);
+
+// Print the character at index 5 of the given string
+console.log("the character at index 5 os :",
+    text.charAt(5)
+);
+
+// Print the ASCII code at index 5 of the given String
+console.log("the ASCII code of the character at index 5 is :",text.charCodeAt(5));
+
+// Print the sliced part of the String
+console.log("The sliced part of the given string is:", text.slice(8,16));
